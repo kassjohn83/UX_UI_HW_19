175566053402dc6cc7541b797b1ed682b23849a8
kassjohn83 <kljohn083@gmail.com>
2024-03-19 21:40:53 -0700

    js

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('.header a').hover(
+        function() {
+            $(this).addClass('hover');
+        },
+        function() {
+            $(this).removeClass('hover');
+        }
+    );
+
+    $('.whitehorse').hover(
+        function() {
+            $(this).addClass('hover');
+        },
+        function() {
+            $(this).removeClass('hover');
+        }
+    );
+    $('.Cuesine').hover(
+        function() {
+            $(this).addClass('hover');
+        },
+        function() {
+            $(this).removeClass('hover');
+        }
+    );
+});
+
