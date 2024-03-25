29f63b86b0120cbaa7d32bb9108a6eabc99dd130
kassjohn83 <kljohn083@gmail.com>
2024-03-25 16:36:36 -0700

    links

--- js/index.js
+++ js/index.js
@@ -16,13 +16,16 @@ $(document).ready(function() {
             $(this).removeClass('hover');
         }
     );
+    $('.Cuesine').click(function() {
+        window.location.href = 'https://kassjohn83.github.io/UX_UI_HW_20/#projects';
+    });
     $('.Cuesine').hover(
         function() {
             $(this).addClass('hover');
         },
         function() {
             $(this).removeClass('hover');
-        }
-    );
+        } 
+        );
 });
 
